b641313a758cf18e90ef154332c8f0fe3578d893
Michael Fabian Dirks <michael.dirks@realitybends.de>
2015-05-10 17:31:34 +0200

    Initial Commit with Data
    Signed-off-by: Michael Fabian Dirks <michael.dirks@realitybends.de>

---
+++ BlitzSteam/BlitzPointer.h
@@ -0,0 +1,9 @@
+#include "dllmain.h"
+
+DLL_EXPORT uint32_t BlitzPointer_GetReturnAddress();
+DLL_EXPORT uint32_t BlitzPointer_GetFunctionPointer();
+DLL_EXPORT uint32_t BlitzPointer_CallFunction0(uint32_t fpFunctionPointer);
+DLL_EXPORT uint32_t BlitzPointer_CallFunction1(uint32_t fpFunctionPointer, uint32_t p1);
+DLL_EXPORT uint32_t BlitzPointer_CallFunction2(uint32_t fpFunctionPointer, uint32_t p1, uint32_t p2);
+DLL_EXPORT uint32_t BlitzPointer_CallFunction3(uint32_t fpFunctionPointer, uint32_t p1, uint32_t p2, uint32_t p3);
+DLL_EXPORT uint32_t BlitzPointer_CallFunction4(uint32_t fpFunctionPointer, uint32_t p1, uint32_t p2, uint32_t p3, uint32_t p4);
---
+++ BlitzSteam/dllmain.cpp
@@ -0,0 +1,16 @@
+#include "dllmain.h"
+
+bool WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
+	switch (fdwReason) {
+		case DLL_PROCESS_ATTACH:
+			break;
+		case DLL_PROCESS_DETACH:
+			break;
+		case DLL_THREAD_ATTACH:
+			break;
+		case DLL_THREAD_DETACH:
+			break;
+	}
+
+	return true;
+}
