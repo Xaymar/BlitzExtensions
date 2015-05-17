7db3cac24829919a4f51f84c13f0b4b578a01f1e
Michael Fabian Dirks <michael.dirks@realitybends.de>
2015-05-17 11:57:11 +0200

    Split BlitzSteam into it's own repository.

--- BlitzSteam/Wrapper/SteamUser.cpp
+++
@@ -1,7 +0,0 @@
-#include "dllmain.h"
-
-DLL_EXPORT void* BlitzSteam_ISteamUser_GetSteamID(void* lpSteamUser) {
-	return (void*)&(((ISteamUser*)lpSteamUser)->GetSteamID());
-}
-
-//DLL_EXPORT void* BlitzSteam_ISteamUser_
